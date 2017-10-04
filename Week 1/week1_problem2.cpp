edaf90bed600ca60e8446db6f5bede72a1e5fae7
Pasha Khosravi <khosravipasha@users.noreply.github.com>
2017-10-04 01:36:26 -0700

    nit

--- Week 1/week1_problem2.cpp
+++ Week 1/week1_problem2.cpp
@@ -6,13 +6,13 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int> > d(n, vector<int>(m));
         
-        for(int i = 0;i < n; i++)
+        for(int i = 0; i < n; i++)
             d[i][0] = 1;
-        for(int j = 0;j < m; j++)
+        for(int j = 0; j < m; j++)
             d[0][j] = 1;
         
-        for(int i = 1;i < n; i++)
-            for(int j = 1;j < m; j++)
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < m; j++)
                 d[i][j] = d[i-1][j] + d[i][j-1];
         
         return d[n-1][m-1];
